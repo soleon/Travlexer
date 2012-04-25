d3cd83f3c3a58ac51a2ae98ab8630b06cc510e3e
leonzhou <soleon@live.com>
2012-04-25 10:57:47 +1000

    Implemented place loading states for place details page.

--- Travlexer.WindowsPhone/ViewModels/PlaceDetailsViewModel.cs
+++ Travlexer.WindowsPhone/ViewModels/PlaceDetailsViewModel.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Collections.Generic;
-using System.Reflection;
+using System.ComponentModel;
+using Codify;
+using Codify.Commands;
+using Codify.Extensions;
 using Codify.ViewModels;
 using Travlexer.Data;
-using Travlexer.WindowsPhone.Infrastructure;
 
 namespace Travlexer.WindowsPhone.ViewModels
 {
@@ -14,9 +16,23 @@ namespace Travlexer.WindowsPhone.ViewModels
         public PlaceDetailsViewModel()
         {
             Data = ApplicationContext.Data.SelectedPlace;
+            Data.PropertyChanged -= OnDataPropertyChanged;
+            Data.PropertyChanged += OnDataPropertyChanged;
+
+            CommandUpdatePlaceInfo = new DelegateCommand(() => ApplicationContext.Data.GetPlaceDetails(Data));
+
+            IsBusy = Data.DataState == DataStates.Busy;
         }
 
         #endregion
+
+
+        #region Commands
+
+        public DelegateCommand CommandUpdatePlaceInfo { get; private set; }
+
+        #endregion
+
 
         #region Public Properties
 
@@ -29,6 +45,35 @@ namespace Travlexer.WindowsPhone.ViewModels
         {
             get { return ApplicationContext.Data.ElementColorMap; }
         }
+
+        public bool IsBusy
+        {
+            get { return _isBusy; }
+            private set { SetProperty(ref _isBusy, value, IsBusyProperty); }
+        }
+
+        private bool _isBusy;
+        private const string IsBusyProperty = "IsBusy";
+
+        #endregion
+
+
+        #region Event Handling
+
+        private void OnDataPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName != Place.DataStateProperty) return;
+            var dataState = Data.DataState;
+            DataStateChanged.ExecuteIfNotNull(dataState);
+            IsBusy = dataState == DataStates.Busy;
+        }
+
+        #endregion
+
+
+        #region Public Events
+
+        public event Action<DataStates> DataStateChanged;
 
         #endregion
     }
