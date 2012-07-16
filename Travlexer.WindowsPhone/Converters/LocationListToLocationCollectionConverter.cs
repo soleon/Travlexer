56dda8d34a52cc2d33ea06f90eabbf605cd9d8ce
leonzhou <soleon@live.com>
2012-07-16 17:47:20 +1000

    Fixed a bug that causes the routes to not show on the map.

--- Travlexer.WindowsPhone/Converters/LocationListToLocationCollectionConverter.cs
+++ Travlexer.WindowsPhone/Converters/LocationListToLocationCollectionConverter.cs
@@ -2,27 +2,25 @@ using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Windows.Data;
+using Codify.Extensions;
 using Microsoft.Phone.Controls.Maps;
 using Travlexer.Data;
 using Travlexer.WindowsPhone.Infrastructure;
+using System.Linq;
 
 namespace Travlexer.WindowsPhone.Converters
 {
-	public class LocationListToLocationCollectionConverter : IValueConverter
+	public class LocationCollectionToLocationCollectionConverter : IValueConverter
 	{
 		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
 		{
-			if (!(value is List<Location>))
+			if (!(value is IEnumerable<Location>))
 			{
 				return null;
 			}
-			var list = (List<Location>)value;
 			var collection = new LocationCollection();
-			foreach (var l in list)
-			{
-				collection.Add(l.ToGeoCoordinate());
-			}
-			return collection;
+            collection.AddRange(((IEnumerable<Location>)value).Select(l => l.ToGeoCoordinate()));
+		    return collection;
 		}
 
 		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
