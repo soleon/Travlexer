9456d556f1fc534f6f4b9c48a8def884632a5392
Leon Zhou <soleon@live.com>
2012-10-30 23:23:58 +1100

    Implemented reduction algorithm to improve map polyline performance.

--- Travlexer.WindowsPhone/Controls/MapPolyline.cs
+++ Travlexer.WindowsPhone/Controls/MapPolyline.cs
@@ -1,36 +1,131 @@
+using System;
 using System.Windows;
 using System.Windows.Media;
+using Codify.Extensions;
+using Microsoft.Phone.Controls.Maps;
 
 namespace Travlexer.WindowsPhone.Controls
 {
-	public class MapPolyline : Microsoft.Phone.Controls.Maps.MapPolyline
-	{
-		#region Stroke
+    public class MapPolyline : Microsoft.Phone.Controls.Maps.MapPolyline
+    {
+        private bool _isLoaded;
 
-		public new Brush Stroke
-		{
-			get { return (Brush) GetValue(StrokeProperty); }
-			set { SetValue(StrokeProperty, value); }
-		}
+        public MapPolyline()
+        {
+            Loaded += (s, e) =>
+            {
+                _isLoaded = true;
+                UpdateLocations();
+            };
+            Unloaded += (s, e) => _isLoaded = false;
+        }
 
-		/// <summary>
-		/// Defines the <see cref="Stroke"/> dependency property.
-		/// </summary>
-		public static readonly DependencyProperty StrokeProperty = DependencyProperty.Register(
-			"Stroke",
-			typeof (Brush),
-			typeof (MapPolyline),
-			new PropertyMetadata(default(Brush), (s, t) => ((MapPolyline)s).OnStrokeChanged((Brush) t.NewValue))
-			);
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            if (ParentMap == null) return;
+            Clip = new RectangleGeometry {Rect = new Rect(0, 0, ParentMap.ViewportSize.Width, ParentMap.ViewportSize.Height)};
+        }
 
-		/// <summary>
-		/// Called when <see cref="Stroke"/> changes.
-		/// </summary>
-		private void OnStrokeChanged(Brush newValue)
-		{
-			base.Stroke = newValue;
-		}
 
-		#endregion
-	}
+        #region Properties
+
+
+        #region Stroke
+
+        public new Brush Stroke
+        {
+            get { return (Brush) GetValue(StrokeProperty); }
+            set { SetValue(StrokeProperty, value); }
+        }
+
+        /// <summary>
+        ///   Defines the <see cref="Stroke" /> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty StrokeProperty = DependencyProperty.Register(
+            "Stroke",
+            typeof (Brush),
+            typeof (MapPolyline),
+            new PropertyMetadata(default(Brush), (s, t) => ((MapPolyline) s).OnStrokeChanged((Brush) t.NewValue))
+            );
+
+        /// <summary>
+        ///   Called when <see cref="Stroke" /> changes.
+        /// </summary>
+        private void OnStrokeChanged(Brush newValue)
+        {
+            base.Stroke = newValue;
+        }
+
+        #endregion
+
+
+        #region ZoomLevel
+
+        public double ZoomLevel
+        {
+            get { return (double) GetValue(ZoomLevelProperty); }
+            set { SetValue(ZoomLevelProperty, value); }
+        }
+
+        /// <summary>
+        ///   Defines the <see cref="ZoomLevel" /> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty ZoomLevelProperty = DependencyProperty.Register(
+            "ZoomLevel",
+            typeof (double),
+            typeof (MapPolyline),
+            new PropertyMetadata(double.MinValue, (s, e) => ((MapPolyline) s).OnZoomLevelChanged()));
+
+        /// <summary>
+        ///   Called when <see cref="ZoomLevel" /> changes.
+        /// </summary>
+        private void OnZoomLevelChanged()
+        {
+            if (Locations.IsNullOrEmpty()) return;
+            UpdateLocations();
+        }
+
+        #endregion
+
+
+        #region Locations
+
+        public new LocationCollection Locations
+        {
+            get { return (LocationCollection) GetValue(LocationsProperty); }
+            set { SetValue(LocationsProperty, value); }
+        }
+
+        /// <summary>
+        ///   Defines the <see cref="Locations" /> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty LocationsProperty = DependencyProperty.Register(
+            "Locations",
+            typeof (LocationCollection),
+            typeof (MapPolyline),
+            new PropertyMetadata(default(LocationCollection), (s, e) => ((MapPolyline) s).OnLocationsChanged((LocationCollection) e.NewValue)));
+
+        /// <summary>
+        ///   Called when <see cref="Locations" /> changes.
+        /// </summary>
+        private void OnLocationsChanged(LocationCollection newValue)
+        {
+            if (ZoomLevel.Equals(double.MinValue)) return;
+            UpdateLocations();
+        }
+
+        #endregion
+
+
+        #endregion
+
+
+        private void UpdateLocations()
+        {
+            if (!_isLoaded || ZoomLevel.Equals(double.MinValue) || Locations.IsNullOrEmpty()) return;
+            var tolorance = Math.Pow(1/ZoomLevel, 3D);
+            base.Locations = Utility.DouglasPeuckerReduction(Locations, tolorance);
+        }
+    }
 }
---
+++ Travlexer.WindowsPhone/Controls/Utility.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Device.Location;
+
+namespace Travlexer.WindowsPhone.Controls
+{
+    internal static class Utility
+    {
+        /// <summary>
+        ///   Uses the Douglas Peucker algorithim to reduce the number of points.
+        /// </summary>
+        internal static T DouglasPeuckerReduction<T>(T points, Double tolerance) where T : class, IList<GeoCoordinate>, new()
+        {
+            if (points == null || points.Count < 3)
+                return points;
+
+            const int firstPoint = 0;
+            var lastPoint = points.Count - 1;
+            var pointIndexsToKeep = new List<int> {firstPoint, lastPoint};
+
+            //Add the first and last index to the keepers
+
+            //The first and the last point can not be the same
+            while (points[firstPoint].Equals(points[lastPoint]))
+                lastPoint--;
+
+            DouglasPeuckerReduction(points, firstPoint, lastPoint, tolerance, ref pointIndexsToKeep);
+
+            var returnPoints = new T();
+            pointIndexsToKeep.Sort();
+            foreach (var index in pointIndexsToKeep)
+                returnPoints.Add(points[index]);
+
+            return returnPoints;
+        }
+
+        private static void DouglasPeuckerReduction(IList<GeoCoordinate> points, int firstPoint, int lastPoint, Double tolerance, ref List<int> pointIndexsToKeep)
+        {
+            Double maxDistance = 0;
+            var indexFarthest = 0;
+
+            for (var index = firstPoint; index < lastPoint; index++)
+            {
+                var distance = PerpendicularDistance(points[firstPoint], points[lastPoint], points[index]);
+                if (!(distance > maxDistance)) continue;
+                maxDistance = distance;
+                indexFarthest = index;
+            }
+
+            if (!(maxDistance > tolerance) || indexFarthest == 0) return;
+            //Add the largest point that exceeds the tolerance
+            pointIndexsToKeep.Add(indexFarthest);
+
+            DouglasPeuckerReduction(points, firstPoint, indexFarthest, tolerance, ref pointIndexsToKeep);
+            DouglasPeuckerReduction(points, indexFarthest, lastPoint, tolerance, ref pointIndexsToKeep);
+        }
+
+        /// <summary>
+        ///   The distance of a point from a line made from point1 and point2.
+        /// </summary>
+        private static Double PerpendicularDistance(GeoCoordinate point1, GeoCoordinate point2, GeoCoordinate point)
+        {
+            //Area = |(1/2)(x1y2 + x2y3 + x3y1 - x2y1 - x3y2 - x1y3)|   *Area of triangle
+            //Base = √((x1-x2)²+(x1-x2)²)                               *Base of Triangle*
+            //Area = .5*Base*H                                          *Solve for height
+            //Height = Area/.5/Base
+
+            var area = Math.Abs(.5*(point1.Latitude*point2.Longitude + point2.Latitude*point.Longitude + point.Latitude*point1.Longitude - point2.Latitude*point1.Longitude - point.Latitude*point2.Longitude - point1.Latitude*point.Longitude));
+            var bottom = Math.Sqrt(Math.Pow(point1.Latitude - point2.Latitude, 2) + Math.Pow(point1.Longitude - point2.Longitude, 2));
+            var height = area/bottom*2;
+
+            return height;
+        }
+    }
+}
