8cf77fd7a87f6a04b7a4eb1449f0dd2f67b92fa7
Leon Zhou <soleon@live.com>
2013-01-21 22:11:18 +1100

    Consolidated ad controls for easier ad provider switch. Also rolled back to admob as smaato's approval process takes forever.

--- Travlexer.WindowsPhone/Controls/AdControl.cs
+++ Travlexer.WindowsPhone/Controls/AdControl.cs
@@ -1,6 +1,7 @@
-﻿using System.ComponentModel;
+﻿using System.Windows;
 using System.Windows.Controls;
-using SOMAWP7;
+using System.Windows.Data;
+using Codify.Converters;
 
 namespace Travlexer.WindowsPhone.Controls
 {
@@ -8,22 +9,45 @@ namespace Travlexer.WindowsPhone.Controls
     {
         public AdControl()
         {
-            var ad = new SomaAdViewer
-            {
-#if DEBUG
-                ShowErrors = true,
-#endif
-                Pub = 923838645,
-                Adspace = 65773647,
-                PopupAd = true,
-                LocationUseOK = true
-            };
-            Content = ad;
+            #region Smaato
 
-#if DEBUG
-            if (DesignerProperties.IsInDesignTool) return;
-#endif
-            ad.StartAds();
+//            {
+//                var ad = new SOMAWP7.SomaAdViewer
+//                {
+//#if DEBUG
+//                    ShowErrors = true,
+//#endif
+//                    Pub = 923838645,
+//                    Adspace = 65773647,
+//                    PopupAd = true,
+//                    LocationUseOK = true
+//                };
+//                Content = ad;
+
+//#if DEBUG
+//                if (System.ComponentModel.DesignerProperties.IsInDesignTool) return;
+//#endif
+//                ad.StartAds();
+//            }
+
+            #endregion
+
+            #region AdMob
+
+            {
+                var ad = new Google.AdMob.Ads.WindowsPhone7.WPF.BannerAd
+                {
+                    AdUnitID = "a150e41c7f6d5e1"
+                };
+                SetBinding(VisibilityProperty, new Binding("AdAvailable")
+                {
+                    RelativeSource = new RelativeSource(RelativeSourceMode.Self),
+                    Converter = (VisibilityConverter)Application.Current.Resources["VisibilityConverter"]
+                });
+                Content = ad;
+            }
+
+            #endregion
         }
     }
 }
